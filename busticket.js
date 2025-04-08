2cc760e8d761f53bf57781b0c5ef13a431dc89ca
Hasibul Hasan <hasibulhasanx0902@gmail.com>
2025-04-08 19:29:18 +0600

    only one append

--- busticket.js
+++ busticket.js
@@ -1,38 +1,44 @@
 function scrollToTicket() {
-scroll('ticket-section')
+  scroll('ticket-section')
 
 }
 
 
 function clickMouse(event) {
- 
+
   const count2 = getIntValue('count-2')
-  
-  if(count2>=4){
+
+  if (count2 >= 4) {
     alert('You can only select up to 4 seats.');
-    
-    return; 
+
+    return;
   }
 
   const press = event.target.innerText
   addClasslist(press)
-  
+
   const count1 = getIntValue('count-1') - 1
   setTextValue('count-1', count1)
 
-  
-  setTextValue('count-2', count2+1)
+
+  setTextValue('count-2', count2 + 1)
 
   const count3 = getIntValue('count-3') + 550
   setTextValue('count-3', count3)
 
+  setTextValue('sit-1',press)
+  setTextValue('eco','Economy')
+  setTextValue('price',550)
+  
+ 
+
   const count4 = getIntValue('count-4') + 550
   setTextValue('count-4', count4)
 
-  document.getElementById('sit-1').innerText = press
+ 
 
-  if(count2==3){document.getElementById('apply').removeAttribute('disabled')} 
-  
+  if (count2 == 3) { document.getElementById('apply').removeAttribute('disabled') }
+
 
 
 }
@@ -45,9 +51,9 @@ function numberInput(element) {
   const input = element.target.value
   const input2 = Number(input)
   const count2 = getIntValue('count-2')
- 
 
-  if (typeof (input2) == 'number'  && !isNaN(input2) && count2 > 0 && input.trim() !== '' ) {
+
+  if (typeof (input2) == 'number' && !isNaN(input2) && count2 > 0 && input.trim() !== '') {
     document.getElementById('nextBtn').removeAttribute('disabled')
   }
 
@@ -59,25 +65,32 @@ function numberInput(element) {
 }
 
 
-document.getElementById('nextBtn').addEventListener('click',success)
+document.getElementById('nextBtn').addEventListener('click', success)
 
-function success(){
+function success() {
   removeHidden('successScreen')
   addHidden('ticket-section')
   addHidden('offerSection')
   addHidden('header')
   addHidden('footer')
-  document.getElementById('phoneNumber').value=''
-  document.getElementById('count-2').innerText=0
-  document.getElementById('count-3').innerText=0
-  document.getElementById('count-4').innerText=0
+
+  document.getElementById('phoneNumber').value = ''
+  document.getElementById('count-2').innerText = 0
+  document.getElementById('count-3').innerText = 0
+  document.getElementById('count-4').innerText = 0
   document.getElementById('apply').classList.remove('hidden')
   document.getElementById('coupon-code').classList.remove('hidden')
-  document.getElementById('apply').setAttribute('disabled')
+  document.getElementById('apply').disabled=true;
+  document.getElementById('coupon-code').value=''
+  setTextValue('sit-1','-')
+  setTextValue('eco','-')
+  setTextValue('price','-')
+
+
 
 }
 
-function againPurchase(){
+function againPurchase() {
   addHidden('successScreen')
   removeHidden('ticket-section')
   removeHidden('offerSection')
@@ -85,37 +98,31 @@ function againPurchase(){
   removeHidden('footer')
   scroll('scroll2')
 
-  
+
+
 }
 
-document.getElementById('coupon-code').addEventListener('keyup',couponField)
-function couponField(element){
-const input= element.target.value
-if(input==='NEW15'){
-  document.getElementById('apply').addEventListener('click',new15)
-  function new15(){
-   const newTotal1= getIntValue('count-4')*0.85
-   setTextValue('count-4',newTotal1)
-  
-   addHidden('apply')
-   addHidden('coupon-code')
+
+
+document.getElementById('apply').addEventListener('click', applyCoupon)
+function applyCoupon() {
+  const input = document.getElementById('coupon-code').value
+
+  if (input === 'NEW15') {
+    const newTotal1 = getIntValue('count-4') * 0.85
+    setTextValue('count-4', newTotal1)
+    addHidden('apply')
+    addHidden('coupon-code')
+  } else if (input === 'Couple20') {
+    const newTotal2 = getIntValue('count-4') * 0.80
+    setTextValue('count-4', newTotal2)
+    addHidden('apply')
+    addHidden('coupon-code')
+  } else if(input === '') {
+    alert('Enter a coupon')
   }
-
-}
-if( input==='Couple20'){
-  document.getElementById('apply').addEventListener('click',new20)
-  
-  function new20(){
-    const newTotal2= getIntValue('count-4')*0.80
-   setTextValue('count-4',newTotal2)
-  
-   addHidden('apply')
-   addHidden('coupon-code')
-
+  else{
+    alert('Wrong coupon')
   }
-  
 }
 
-
-
-}
